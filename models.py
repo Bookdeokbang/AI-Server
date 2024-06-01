2ff447e40e7e9777d0c06677c7dde88d2e1d9f0e
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-06-02 01:43:20 +0900

    model info 조회 관련 기능 추가

--- models.py
+++ models.py
@@ -11,3 +11,20 @@ class AIModel(str, Enum):
     ROBERTA = "roberta"
     ALBERT = "albert"
     SPANBERT = "spanbert"
+
+
+class ModelInfo(BaseModel):
+    status: str
+    model_name: str
+    num_parameters: int
+    num_layers: int
+    hidden_size: int
+    num_attention_heads: int
+    vocab_size: int
+    max_position_embeddings: int
+    type_vocab_size: int
+    initializer_range: float
+    layer_norm_eps: float
+    pad_token_id: int
+    bos_token_id: int
+    eos_token_id: int
--- routers.py
+++ routers.py
@@ -4,12 +4,11 @@ from fastapi import APIRouter, HTTPException
 from typing import List
 
 from google.oauth2 import service_account
-from sympy.geometry import entity
 from transformers import AutoTokenizer, AutoModelForTokenClassification, TokenClassificationPipeline, pipeline
 
-from models import AIModel
+from models import AIModel, ModelInfo
 from bert import bert_classification
-from roberta import roberta_classification, create_ten_sentence, choice_one_sentence
+from roberta import roberta_classification, create_ten_sentence, choice_one_sentence, choice_roberta_model
 from albert import albert_classification
 from google.cloud import vision
 from models import OCRResponse
@@ -75,7 +74,62 @@ async def pos(text: str):
     response = {}
     for v in pos_results:
         word = v['word']
-        entity = pos_tags.get(v['entity'], "Unknown")
-        response[word] = entity
+        entity = v['entity']
+
+        # 키가 점(.)으로 시작하지 않는 경우에만 처리
+        if not word.startswith('.'):
+            pos_tag = pos_tags.get(entity, "Unknown")
+            response[word] = pos_tag
 
     return response
+
+@predict_router.get("/model_info", response_model=ModelInfo)
+async def get_model_info():
+    tokenizer, _, model = choice_roberta_model()
+    if model and tokenizer:
+        status = "Model loaded successfully"
+        model_name = model.config.name_or_path
+        num_parameters = model.num_parameters()
+        num_layers = model.config.num_hidden_layers
+        hidden_size = model.config.hidden_size
+        num_attention_heads = model.config.num_attention_heads
+        vocab_size = model.config.vocab_size
+        max_position_embeddings = model.config.max_position_embeddings
+        type_vocab_size = model.config.type_vocab_size
+        initializer_range = model.config.initializer_range
+        layer_norm_eps = model.config.layer_norm_eps
+        pad_token_id = model.config.pad_token_id
+        bos_token_id = model.config.bos_token_id
+        eos_token_id = model.config.eos_token_id
+    else:
+        status = "Model not loaded"
+        model_name = ""
+        num_parameters = 0
+        num_layers = 0
+        hidden_size = 0
+        num_attention_heads = 0
+        vocab_size = 0
+        max_position_embeddings = 0
+        type_vocab_size = 0
+        initializer_range = 0.0
+        layer_norm_eps = 0.0
+        pad_token_id = 0
+        bos_token_id = 0
+        eos_token_id = 0
+
+    return {
+        "status": status,
+        "model_name": model_name,
+        "num_parameters": num_parameters,
+        "num_layers": num_layers,
+        "hidden_size": hidden_size,
+        "num_attention_heads": num_attention_heads,
+        "vocab_size": vocab_size,
+        "max_position_embeddings": max_position_embeddings,
+        "type_vocab_size": type_vocab_size,
+        "initializer_range": initializer_range,
+        "layer_norm_eps": layer_norm_eps,
+        "pad_token_id": pad_token_id,
+        "bos_token_id": bos_token_id,
+        "eos_token_id": eos_token_id
+    }
